3d030f09e1b02f8fd7aa501513a0041da2521091
eugenelipsky <zvlipskiy@mail.ru>
2020-03-17 23:58:13 +0200

    About, Sevices, Info sections added

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,27 @@
 $(function(){
 
+    $(document).ready(function() {
+        $('.header__content-video').magnificPopup({
+            disableOn: 300,
+            type: 'iframe',
+            mainClass: 'mfp-fade',
+            removalDelay: 160,
+            preloader: false,
+            fixedContentPos: false
+        });
+    });
+
+    $('.header__search-btn').on('click', function () {
+        $('.search_bar').slideToggle();
+    })
+
+    $('.header__menu_btn').on('click', function () {
+        $('.menu__list').slideToggle();
+    })
+
+    $('.services__slider').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        variableWidth: true,
+    });
 });
